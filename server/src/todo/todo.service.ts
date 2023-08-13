b20e004a71de91b3e2f86c6046ba6c2d8ef79adf
Deleanu Radu <radu_deleanu@outlook.com>
2023-08-14 01:06:24 +0300

    basic crud

---
+++ server/src/todo/todo.service.ts
@@ -0,0 +1,43 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TodoEntity } from './entity/todo.entity';
+import { TodoDto } from './dto/todo.dto';
+import { todos } from 'src/mock/todos.mock';
+import { toPromise } from '../shared/utils';
+import { toTodoDto } from '../shared/mapper';
+import { TodoCreateDto } from './dto/todo.create.dto';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class TodoService {
+  todos: TodoEntity[] = todos;
+
+  async getAllTodo(): Promise<TodoDto[]> {
+    return this.todos.map((todo) => toTodoDto(todo));
+  }
+
+  getOneTodo(id: string): Promise<TodoDto> {
+    const todo = this.todos.find((todo) => todo.id === id);
+
+    if (!todo) {
+      throw new HttpException(
+        `Todo item doesn't exist`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return toPromise(toTodoDto(todo));
+  }
+
+  async createTodo(todoDto: TodoCreateDto): Promise<TodoDto> {
+    const { name, description } = todoDto;
+
+    const todo: TodoEntity = {
+      id: uuid(),
+      name,
+      description,
+    };
+
+    this.todos.push(todo);
+    return toPromise(toTodoDto(todo));
+  }
+}
