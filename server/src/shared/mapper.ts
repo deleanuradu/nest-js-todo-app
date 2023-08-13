2a7d2f1349bf07d2347d1b93ed94f914ba8daa9e
Deleanu Radu <radu_deleanu@outlook.com>
2023-08-13 19:30:23 +0300

    added todo module

---
+++ server/src/shared/mapper.ts
@@ -0,0 +1,9 @@
+import { TodoDto } from '../todo/dto/todo.dto';
+import { TodoEntity } from '../todo/entity/todo.entity';
+
+export const toTodoDto = (data: TodoEntity): TodoDto => {
+  const { id, name, description } = data;
+
+  const todoDto: TodoDto = { id, name, description };
+  return todoDto;
+};
