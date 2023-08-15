178e6e6f39d6be11ba60630910c7845a9670cf07
Deleanu Radu <radu_deleanu@outlook.com>
2023-08-16 01:05:44 +0300

    docker integration and db

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import 'dotenv/config';
+import 'reflect-metadata';
 
 const port = process.env.PORT;
 
@@ -9,5 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 
-  Logger.log(`Server started running onhttp://localhost:${port}`, 'Bootstrap');}
+  Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
+}
+
 bootstrap();
